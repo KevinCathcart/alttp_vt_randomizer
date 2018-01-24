d4621cb8316163652463ef3f8d19ffc673fd9d15
Kevin Cathcart <kevincathcart@gmail.com>
2018-01-24 16:30:00 -0500

    Make RandomBeatable use RandomAssumed Algorithm.
    Update the RandomBeatable filler to be based on the less biased RandomAssumed algorithm.
    It is now basically equivalent to Entrance Randomizer's `--only-beatable` option.
    
    Basically whenever we are about to place an item, we will check if the game would be beatable without that item (taking into account where existing items were placed). If the game would be beatable without it, we allow it to be placed anywhere, subject only to Fill Rule constraints. (i.e. without checking if the location would be accessible).

--- app/Filler/RandomBeatable.php
+++ app/Filler/RandomBeatable.php
@@ -18,85 +18,51 @@ class RandomBeatable extends Random {
 	public function fill(array $dungeon, array $required, array $nice, array $extra) {
 		// back hack from RandomAssumed
 		$randomized_order_locations = $this->shuffleLocations($this->world->getEmptyLocations());
-		$this->fillDungeonItemsInLocations($dungeon, $randomized_order_locations, $required);
-
-		$randomized_order_locations = $this->shuffleLocations($this->world->getEmptyLocations());
-
-		$my_items = $this->world->collectItems();
-
+			
+		$this->fillItemsInLocations($dungeon, $randomized_order_locations, array_merge($required, $nice));
+		
+		// random junk fill
+		$gt_locations = $this->world->getRegion('Ganons Tower')->getEmptyLocations()->randomCollection(mt_rand(0, 15));
+		$extra = $this->shuffleItems($extra);
+		$trash = array_splice($extra, 0, $gt_locations->count());
+		$this->fastFillItemsInLocations($trash, $gt_locations);
+		
+		$randomized_order_locations = $randomized_order_locations->getEmptyLocations()->reverse();
+		
 		// Random beatable might require the nice to haves (and even the trash later)
-		$items = $this->shuffleItems(array_merge($required, $nice));
-		$this->fillItemsInLocations($items, $my_items, $randomized_order_locations, true);
+		$this->fillItemsInLocations($this->shuffleItems(array_merge($required, $nice)), $randomized_order_locations);
 
-		$randomized_order_locations = $this->shuffleLocations($this->world->getEmptyLocations());
+		$randomized_order_locations = $this->shuffleLocations($randomized_order_locations->getEmptyLocations());
 		$this->fastFillItemsInLocations($this->shuffleItems($extra), $randomized_order_locations);
 	}
 
-	protected function fillItemsInLocations($fill_items, $my_items, $locations, $check_for_new_locations = false) {
-		Log::debug(sprintf("Filling %s items in %s locations", count($fill_items), $locations->getEmptyLocations()->count()));
-		$total_items = count($fill_items);
-		reset($fill_items);
-		while (count($fill_items) && $locations->getEmptyLocations()->count()) {
-			$item = current($fill_items);
+	protected function fillItemsInLocations($fill_items, $locations, $base_assumed_items = []) {
+		$remaining_fill_items = new Items($fill_items);
+		Log::debug(sprintf("Filling %s items in %s locations", $remaining_fill_items->count(),
+			$locations->getEmptyLocations()->count()));
 
-			$available_locations = $locations->getEmptyLocations()->filter(function($location) use ($my_items) {
-				return $location->canAccess($my_items);
-			});
+		if ($remaining_fill_items->count() > $locations->getEmptyLocations()->count()) {
+			throw new \Exception("Trying to fill more items than available locations.");
+		}
 
-			$fillable_locations = $available_locations->filter(function($location) use ($item, $my_items) {
-				return $location->canFill($item, $my_items);
+		foreach ($fill_items as $key => $item) {
+			$assumed_items = $this->world->collectItems($remaining_fill_items->removeItem($item->getName())->merge($base_assumed_items));
+
+			$perform_access_check = !$this->world->getWinCondition()($assumed_items);
+
+			$fillable_locations = $locations->filter(function($location) use ($item, $assumed_items, $perform_access_check) {
+				return !$location->hasItem() && $location->canFill($item, $assumed_items, $perform_access_check);
 			});
 
 			if ($fillable_locations->count() == 0) {
-				foreach ($locations->getEmptyLocations() as $log_loc) {
-					Log::error("SOFT LOCK LOCATION: " . $log_loc->getName());
-				}
 				throw new \Exception(sprintf('No Available Locations: "%s"', $item->getNiceName()));
 			}
 
-			Log::debug(sprintf("Item: %s [%s] Locations: %s of %s",
-				$item->getNiceName(), $item->getName(), $locations->getEmptyLocations()->count(), $available_locations->count()));
+			$fill_location = $fillable_locations->first();
 
-			if ($check_for_new_locations) {
-				$my_new_items = $my_items->tempAdd($item);
-
-				$available_after_placement = $locations->getEmptyLocations()->filter(function($location) use ($my_new_items) {
-					return $location->canAccess($my_new_items);
-				});
-
-				Log::debug(sprintf("Before: %s After: %s", $available_locations->count(), $available_after_placement->count()));
-				if ($available_after_placement->count() <= $available_locations->count()) {
-					if (next($fill_items) !== false) {
-						Log::debug(sprintf("Skipping Item: %s [%s]", $item->getNiceName(), $item->getName()));
-						continue;
-					} else {
-						end($fill_items);
-					}
-				}
-			}
-
-			$fill_location = (count($fill_items) / $total_items <= .25)
-				? $fillable_locations->first()
-				: $fillable_locations->last();
 			Log::debug(sprintf("Placing Item: %s in %s", $item->getNiceName(), $fill_location->getName()));
+
 			$fill_location->setItem($item);
-
-			unset($fill_items[key($fill_items)]);
-			reset($fill_items);
-
-			$my_items = $this->world->collectItems();
-			if ($this->world->getWinCondition()($my_items)) {
-				Log::debug("Beatable!");
-				break;
-			}
-		}
-
-		foreach ($fill_items as $item) {
-			Log::debug(sprintf('Left over: %s', $item->getNiceName()));
-		}
-
-		if (count($fill_items) && $locations->getEmptyLocations()->count()) {
-			$this->fastFillItemsInLocations($fill_items, $locations->getEmptyLocations());
 		}
 	}
 }
